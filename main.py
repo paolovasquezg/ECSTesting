a1f4a2056c3a7865dbef5939c2dfa96902716a88
Paolo Vásquez <83627663+paolovasquezg@users.noreply.github.com>
2026-06-17 14:44:12 -0500

    testing ci/cd

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Testing"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
